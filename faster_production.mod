0e0c3168b7aeacd3f1b40baf3947de3d24764ebd
shijiudada <59414654+shijiudadayyds@users.noreply.github.com>
2024-11-01 21:53:08 +0800

    Add files via upload

---
+++ faster_production.mod
@@ -0,0 +1,7 @@
+name="Faster Production Mod"
+path="mod/faster_production"
+tags={
+    "Gameplay"
+    "Balance"
+}
+supported_version="1.14.10"  # 此处请确认填写你的游戏版本
---
+++ faster_production/common/defines/defines.lua
@@ -0,0 +1,4 @@
+NDefines.NProduction.BASE_FACTORY_SPEED = 50.0
+NDefines.NProduction.BASE_FACTORY_EFFICIENCY_GAIN = 0.5
+NDefines.NProduction.BASE_FACTORY_MAX_EFFICIENCY_FACTOR = 2.0
+NDefines.NGame.STARTUP_DELAYED_EVENTS = "faster_production.1"  -- 游戏启动时触发事件
